cd693b92ab782dc7999d062c76988a7467f450a0
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-14 10:20:01 -0600

    direction

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -3,14 +3,20 @@
 ************************************* */
 
 
-const feelTemp = document.getElementById('feelTemp');
+
 
 /*Variables for Function Use*/
 const temp = 31;
 const speed = 5;
 buildWC(speed, temp);
+
+const direction = "N"; /*Set your own value*/
+windDial(direction);
+
 /* Calculate the Windchill*/
 function buildWC(speed, temp) {
+
+    const feelTemp = document.getElementById('feelTemp');
 
     /*Compute the windchill*/
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
@@ -27,6 +33,55 @@ function buildWC(speed, temp) {
       // Display the windchill
       console.log(wc);
       feelTemp.innerHTML = wc;
+}
+
+
+
+/*Wind Dial Function*/
+function windDial(direction){
+
+// Get the wind dial container
+const dial = document.getElementById("dial");
+
+    /*Determine the dial class*/
+    switch (direction){
+    case "North":
+    case "N":
+     dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+     break;
+    case "NE":
+    case "NNE":
+    case "ENE":
+     dial.setAttribute("class", "ne");
+     break;
+    case "NW":
+    case "NNW":
+    case "WNW":
+     dial.setAttribute("class", "nw");
+     break;
+    case "South":
+    case "S":
+     dial.setAttribute("class", "s");
+     break;
+    case "SE":
+    case "SSE":
+    case "ESE":
+     dial.setAttribute("class", "se");
+     break;
+    case "SW":
+    case "SSW":
+    case "WSW":
+     dial.setAttribute("class", "sw");
+     break;
+    case "East":
+    case "E":
+     dial.setAttribute("class", "e");
+     break;
+    case "West":
+    case "W":
+     dial.setAttribute("class", "w");
+     break;
+   }
 }
 
 
