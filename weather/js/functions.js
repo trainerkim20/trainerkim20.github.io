6947431b6f8436ed4022df82e26698706a2ca32e
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-14 09:40:30 -0600

    math

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -2,4 +2,19 @@
 *  Weather Site JavaScript Functions
 ************************************* */
 
+
+const feelTemp = document.getElementById('feelTemp');
+/* Calculate the Windchill*/
+function buildWC(speed, temp) {
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+}
+
+
+
+
+
+
+
+
 console.log('My javascript is being read.');
