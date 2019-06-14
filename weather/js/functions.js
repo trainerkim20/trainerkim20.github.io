9b0a87bb05fd99621d74098f0fe51e76b4498e57
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-14 09:53:10 -0600

    functions

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -4,10 +4,29 @@
 
 
 const feelTemp = document.getElementById('feelTemp');
+
+/*Variables for Function Use*/
+const temp = 31;
+const speed = 5;
+
 /* Calculate the Windchill*/
 function buildWC(speed, temp) {
+
+    /*Compute the windchill*/
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
+
+  
+
+    /*Round the answer down to integer*/
+    wc = Math.floor(wc);
+
+    /*If chill is greater than temp, return the temp*/
+    wc = (wc > temp)?temp:wc;
+
+      // Display the windchill
+      console.log(wc);
+      feelTemp.innerHTML = wc;
 }
 
 
