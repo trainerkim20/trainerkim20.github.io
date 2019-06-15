9411bc9bf84ba792b80c3ea0e6b4763383bfff47
Kim Freeman <Kim@Kims-MacBook-Pro.local>
2019-06-15 16:22:41 -0600

    changes

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -10,6 +10,18 @@ const temp = 31;
 const speed = 5;
 buildWC(speed, temp);
 
+const direction = "N"; /*Set your own value*/
+windDial(direction);
+
+const condition = "sunny"; 
+getCondition(condition);
+
+const image = "sunny"
+changeSummaryImage(image);
+
+const meters=1514.246;
+convertMeters(meters);
+
 
 
 /* Calculate the Windchill*/
@@ -34,8 +46,7 @@ function buildWC(speed, temp) {
       feelTemp.innerHTML = wc;
 }
 
-const direction = "S"; /*Set your own value*/
-windDial(direction);
+
 
 /*Wind Dial Function*/
 function windDial(direction){
@@ -47,39 +58,48 @@ const dial = document.getElementById("dial");
     switch (direction){
     case "North":
     case "N":
+    
      dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+     winddirection.innerHTML = "N";
      break;
     case "NE":
     case "NNE":
     case "ENE":
      dial.setAttribute("class", "ne");
+     winddirection.innerHTML = "NE";
      break;
     case "NW":
     case "NNW":
     case "WNW":
      dial.setAttribute("class", "nw");
+     winddirection.innerHTML = "NW";
      break;
     case "South":
     case "S":
      dial.setAttribute("class", "s");
+     winddirection.innerHTML = "S";
      break;
     case "SE":
     case "SSE":
     case "ESE":
      dial.setAttribute("class", "se");
+     winddirection.innerHTML = "SE";
      break;
     case "SW":
     case "SSW":
     case "WSW":
      dial.setAttribute("class", "sw");
+     winddirection.innerHTML = "SW";
      break;
     case "East":
     case "E":
      dial.setAttribute("class", "e");
+     winddirection.innerHTML = "E";
      break;
     case "West":
     case "W":
      dial.setAttribute("class", "w");
+     winddirection.innerHTML = "W";
      break;
    }
 }
@@ -88,50 +108,75 @@ const dial = document.getElementById("dial");
 const cd = 'Partly Cloudy';
 const fg = 'Foggy';
 const rn = 'Wet';
-const sow = 'Snowy';
-getCondition(cr, cd, fg, rn, sow);*/ 
+const sow = 'Snowy';*/
+
+let head = document.getElementById("summary").innerHTML;
+let condition = head.toLowerCase();
 
 function getCondition(condition){
-  const summary = document.getElementById("summary");
-switch (condition){
-  case "sunny":
-    return "Clear";
-        case "Partly cloudy":
-       return "Cloudy";
-           case "wet":
-      return "Rain";
-       case "snowy":
-      return "Snow";
-}
-}
-
-function changeSummaryImage(image){
-
-  const forecast = document.getElementById("forecast");
-
-  switch (image){
-    case "sunny":
-   squares.setAttribute("class", "clear");
-   break;
-   case "cloudy":
-  squares.setAttribute("class", "cloud");
-  break;
-  case "cloudy":
-      squares.setAttribute("class", "fog");
-      break;
-      case "wet":
-  squares.setAttribute("class", "rain");
-  break;
-  case "snowy":
-  squares.setAttribute("class", "snow");
-  break;
-  }
   
+if (condition.includes('sunny') || condition.includes('clear') || condition.includes('warm')) {
+  return "clear"; 
+}
+if (condition.includes('partly cloudy') || condition.includes('shady') || condition.includes('cloud')) {
+  return "cloud";
+}
 
+if (condition.includes('wet') || condition.includes('rain') || condition.includes('Rainstorm')) {
+  return "rain";
+}
 
+if (condition.includes('fog') || condition.includes('misty') || condition.includes('hazzy')) {
+  return "fog";
+}
+if (condition.includes('snowstorm') || condition.includes('snow') || condition.includes('freezing')) {
+  return "snow";
+}
+}
+
+let image= getCondition(condition);
+
+changeSummaryImage(image);
+
+function changeSummaryImage(image) {
+
+  const rectangles = document.getElementById("rectangles");
+  const imagesummary = document.getElementById("imagesummary");
+
+  if (image == 'clear'){
+        rectangles.setAttribute("class", "clear");
+        imagesummary.setAttribute("class", "clear");
+  }
+  if (image == 'cloud'){
+      rectangles.setAttribute("class", "cloud");
+      imagesummary.setAttribute("class", "cloud");
+  }
+  if (image == 'fog'){
+      rectangles.setAttribute("class", "fog");
+      imagesummary.setAttribute("class", "fog");
+  }
+  if (image == 'rain'){
+          rectangles.setAttribute("class", "rain");
+          imagesummary.setAttribute("class", "rain");
+  }
+  if (image == 'snow'){
+      rectangles.setAttribute("class", "snow");
+      imagesummary.setAttribute("class", "snow");
+  }
 }
 
 
+function convertMeters(meters){
+
+  let feet= meters*3.281;
+  console.log(feet);
+
+  feet=Math.floor(feet);
+  console.log(feet);
+
+  elevation.innerHTML= feet; 
+
+}
 
 
 
