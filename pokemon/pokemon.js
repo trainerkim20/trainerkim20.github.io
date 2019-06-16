17cd45bc3eeec52ec911c3ff9892e198ae3e851c
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-16 00:17:46 -0600

    hide show

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -1,9 +1,10 @@
 function showData(){
+  /*document.getElementById("grassdata").style.display = "block";*/
     var x = document.getElementById("grassdata");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
+    
   }
-}
