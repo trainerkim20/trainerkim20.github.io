15bd5e389d14296e91baa8339f6846ad0b810562
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-17 15:10:40 -0600

    fighting

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -133,3 +133,20 @@ function showData1(grass){
       }
       
     }
+
+    let fighting = document.getElementById("fightingdata");
+  showData(fighting);
+
+  function showData9(fighting){
+   
+    const flip = document.getElementById("flip9");
+      var x = document.getElementById("fightingdata");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        flip.setAttribute("class", "open");
+      } else {
+        x.style.display = "none";
+        flip.setAttribute("class", "normal");
+      }
+      
+    }
