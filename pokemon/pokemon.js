dc3843b5f67de0f830f8b9c9f9ea7b0bddd3d016
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-16 22:07:19 -0600

    water

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -2,9 +2,9 @@ let grass = document.getElementById("grassdata");
 showData(grass);
 
 
-function showData(grass){
+function showData1(grass){
  
-  const flip1 = document.getElementById("flip1");
+  const flip = document.getElementById("flip1");
     var x = document.getElementById("grassdata");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -18,16 +18,34 @@ function showData(grass){
  let fire = document.getElementById("firedata");
   showData(fire);
 
-  function showData(fire){
+  function showData2(fire){
    
-    const flip2 = document.getElementById("flip2");
+    const flip = document.getElementById("flip2");
       var x = document.getElementById("firedata");
       if (x.style.display === "none") {
         x.style.display = "block";
-        flip2.setAttribute("class", "open");
+        flip.setAttribute("class", "open");
       } else {
         x.style.display = "none";
-        flip2.setAttribute("class", "normal");
+        flip.setAttribute("class", "normal");
       }
       
     }
+
+    let water = document.getElementById("waterdata");
+  showData(water);
+
+  function showData3(water){
+   
+    const flip = document.getElementById("flip3");
+      var x = document.getElementById("firedata");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        flip.setAttribute("class", "open");
+      } else {
+        x.style.display = "none";
+        flip.setAttribute("class", "normal");
+      }
+      
+    }
+
