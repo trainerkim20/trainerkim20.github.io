dd2cc24031e671b514878085915b31dedcb6e22d
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-17 16:51:55 -0600

    rock

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -269,3 +269,20 @@ function showData1(){
       
     }
 
+    let rock = document.getElementById("rockdata");
+  showData(rock);
+
+  function showData17(){
+   
+    const flip = document.getElementById("flip17");
+      var x = document.getElementById("rockdata");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        flip.setAttribute("class", "open");
+      } else {
+        x.style.display = "none";
+        flip.setAttribute("class", "normal");
+      }
+      
+    }
+
