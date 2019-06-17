bd28ff5103ecbae21f54b73dc4436ca56c41e55f
Kim Freeman <Kim@205-185-99-154.directcom.com>
2019-06-16 20:17:17 -0600

    flip

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -3,8 +3,10 @@ function showData(){
     var x = document.getElementById("grassdata");
     if (x.style.display === "none") {
       x.style.display = "block";
+      flip.setAttribute("class", "open");
     } else {
       x.style.display = "none";
+      flip.setAttribute("class", "normal");
     }
     
   }
