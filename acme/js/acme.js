87b1dd5f5705d4651fd061b6ce929c71d6b168c6
Kim Freeman <Kim@Kims-MacBook-Pro.local>
2019-07-16 22:38:19 -0600

    stuf is done

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -15,6 +15,49 @@ var storage = window.localStorage;
 
 let acmeURL = "../js/acme.json";
 let pageNav = document.getElementById('pagenav');
+let home = document.getElementById('homepage');
+let contentContainer = document.getElementById('content');
+
+let productData = JSON.parse(storage.getItem("Object Name"));
+navigationBar(productData);
+function navigationBar(productData) {
+    
+let navListItems = "<li>" + "<a>" + "Home" + "</a>" + "</li>";
+
+    for (let i = 0, x = 4; i < x; i++) {
+   
+        navListItems += '<li>' + "<a>" + productData[i] + "</a>" + '</li>';
+       }
+       return navListItems;
+}
+
+document.getElementById("pageNavItems").innerHTML = navigationBar(productData);
+pageNav.addEventListener('click', function (evt) {
+    home.setAttribute('class', 'hide'); // hides the home container   
+
+    let pageName = evt.target.innerHTML;
+    console.log(pageName);
+    console.log(pageName);
+    // Get the city name
+    switch (pageName) {
+        case "Home":
+                contentContainer.setAttribute('class', 'hide'); // removes the hide class 
+                home.setAttribute('class', '');
+                break;
+        case "Anvils":
+                contentContainer.setAttribute('class', ''); // removes the hide class 
+                break;
+        case "Explosives":
+                contentContainer.setAttribute('class', ''); // removes the hide class 
+                break;
+        case "Decoys":
+                contentContainer.setAttribute('class', ''); // removes the hide class 
+                break;
+        case "Traps":
+                contentContainer.setAttribute('class', ''); // removes the hide class 
+            evt.preventDefault();
+            break;
+    }
 
 getData();
 function getData() {
@@ -32,45 +75,33 @@ function getData() {
 let objectNames = Object.keys(data);
 storage.setItem("Object Name", JSON.stringify(objectNames));
 
-            let g = data;
+            let g = data[pageName];
             storage.setItem("JSON", JSON.stringify(g));
 
+            let productname = g.name;
+            document.getElementById("heading").innerHTML = productname;
+
+            let image = g.path;
+            document.getElementById("image").src = image;
+
+            let description = g.description;
+            document.getElementById("description").innerHTML = description;
+
+            let manufacturer = g.manufacturer;
+            document.getElementById("manufacturer").innerHTML = manufacturer;
+
+            let review = g.reviews;
+            document.getElementById("reviews").innerHTML = review;
+
+            let price = g.price;
+            document.getElementById("price").innerHTML = price;
 
         })
 }
 // let home = document.getElementById('home');
 //     home.innerHTML= "Home";
     //, url("template.html");
-let productData = JSON.parse(storage.getItem("Object Name"));
-navigationBar(productData);
-function navigationBar(productData) {
-    
-let navListItems = "<li>" + "<a>" + "Home" + "</a>" + "</li>";
 
-    for (let i = 0, x = 4; i < x; i++) {
-   
-        navListItems += '<li>' + "<a>" + productData[i] + "</a>" + '</li>';
-       }
-       return navListItems;
-}
-
-document.getElementById("pageNavItems").innerHTML = navigationBar(productData);
-
-
-pageNav.addEventListener('click', function (evt) {
-
-    let pageName = evt.target.innerHTML;
-    console.log(pageName);
-    console.log(pageName);
-    // Get the city name
-    switch (pageName) {
-        case "Anvils":
-        case "Explosives":
-        case "Decoys":
-        case "Traps":
-            evt.preventDefault();
-            break;
-    }
 
 
 
@@ -90,4 +121,5 @@ pageNav.addEventListener('click', function (evt) {
             let testname = g.name;
             console.log(testname);
         })
-})
+        
+    })
