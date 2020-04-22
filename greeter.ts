006f49e9cfdaf0b35edf899dff593c2a877caf39
trainerkim20 <trainerkin20@gmail.com>
2020-04-22 16:39:29 -0600

    commented for notes

--- greeter.ts
+++ greeter.ts
@@ -1,19 +1,19 @@
-class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
-}
+// class Student {
+//     fullName: string;
+//     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//         this.fullName = firstName + " " + middleInitial + " " + lastName;
+//     }
+// }
 
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
 
-function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
 
-let user = new Student("Jane", "M.", "User");
+// let user = new Student("Jane", "M.", "User");
 
-document.body.textContent = greeter(user);
+// document.body.textContent = greeter(user);
